0d5d7e724ed8cfa637665d23f5fa87d4912e5ad3
joshjosey <156711944+joshjosey@users.noreply.github.com>
2024-09-11 12:23:23 -0500

    Added dynamic value that changes with slider

--- vftvk-Simple-Interest-Calculator-master/vftvk-Simple-Interest-Calculator-master/script.js
+++ vftvk-Simple-Interest-Calculator-master/vftvk-Simple-Interest-Calculator-master/script.js
@@ -4,3 +4,7 @@ function compute()
     
 }
         
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
